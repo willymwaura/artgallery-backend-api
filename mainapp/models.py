b7fc15ea5e4e774499bcca809f7267f7f136cf3b
willymwaura <wilsonmwaura697@gmail.com>
2022-03-30 10:48:46 +0300

    add

--- mainapp/models.py
+++ mainapp/models.py
@@ -28,10 +28,9 @@ class Cart(models.Model):
     price=models.IntegerField(default=400)
     imageurl=models.URLField(max_length=200)
     category=models.CharField(max_length=100)
-    designer=models.CharField(max_length=100)
-    designer_id=models.IntegerField()
-    designer_name=models.CharField(max_length=100)
-    description=models.CharField(max_length=100)
+    designer_id=models.IntegerField(null=True)
+    designer_name=models.CharField(max_length=100,null=True)
+    description=models.CharField(max_length=100,null=True)
 
     def __str__(self):
         return '{}{}'.format(self.name,self.category)
